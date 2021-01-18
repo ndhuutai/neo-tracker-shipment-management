9cdbc962717d80272bd7b6a3775d1a3afe50475c
Tai N <ndhuutai1@gmail.com>
2021-01-17 21:02:27 -0800

    added initial Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,22 @@
 import React, {PropsWithChildren} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {createStyles, makeStyles, Theme} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => {
+    return createStyles({
+        root: {
+            height: '100vh',
+            width: '100vw'
+        }
+    })
+})
 
 export default ({children} : PropsWithChildren<{}>) => {
+    const classes = useStyles();
     return (
-        <>
+        <div className={classes.root}>
             <CssBaseline/>
             {children}
-        </>
+        </div>
     )
 }
