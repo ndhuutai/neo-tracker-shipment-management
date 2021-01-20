9a630bd14e7df10c08990554b64164ca94bd7520
Tai N <ndhuutai1@gmail.com>
2021-01-19 17:33:48 -0800

    added browser history to initiate store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
+import {createBrowserHistory} from 'history'
 import store from './store/configureStore'
 
 import App from "./App";
 
+const browserHistory = createBrowserHistory();
+
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={store(browserHistory)}>
         <App/>
     </Provider>, document.getElementById("root"));
