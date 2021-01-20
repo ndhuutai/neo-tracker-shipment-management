373890a2684084050c06afb6768cd259cfa04821
Tai N <ndhuutai1@gmail.com>
2021-01-19 22:13:52 -0800

    moving store initialization to configureStore

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
-import {createBrowserHistory} from 'history'
 import store from './store/configureStore'
 
 import App from "./App";
 
-const browserHistory = createBrowserHistory();
-
 ReactDOM.render(
-    <Provider store={store(browserHistory)}>
+    <Provider store={store}>
         <App/>
     </Provider>, document.getElementById("root"));
